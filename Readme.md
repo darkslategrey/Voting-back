a9b422f3fd7dbc85c117c065b504af8a55def1cb
Gregory Faruch <gregory.faruch@web-atrio.com>
2023-02-03 03:00:05 +0100

    update readme

--- Readme.md
+++ Readme.md
@@ -1,3 +1,11 @@
 # Voting Dapp
 
 We mix hardhat et truffle configs to make all tests pass.
+
+Hardhat tests
+
+`hh-tests`
+
+Truffle tests
+
+`truffle-test`
--- scripts/get-voters.js
+++ scripts/get-voters.js
@@ -4,7 +4,7 @@ async function main() {
   const accounts = await hre.ethers.getSigners();
   // const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
   //
-  const contractAddress = "0x303943B1Da92C7Fa710Af5Cd2fC438f9B5885593";
+  const contractAddress = "0xdE64C1Ee54CcEfc0144d2F13747aa9bD3c058cdD";
   const myContract = await hre.ethers.getContractAt(
     "Voting",
     contractAddress,
